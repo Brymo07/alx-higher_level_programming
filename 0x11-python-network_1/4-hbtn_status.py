717f005cc89a8d43735ddef2c24d543cb92f8c02
Brymo07 <felixowusu479@gmail.com>
2023-03-31 19:13:30 -0700

    network

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,8 +5,7 @@ Python script that fetches an URL with requests package
 import requests
 
 if __name__ == "__main__":
-    r = requests.get("https://alx-intranet.hbtn.io/status")
-    print("Body response:")
-    print("\t- type: {}".format(type(r.text)))
-    print("\t- content: {}".format(r.text))
-
+    response = requests.get('https://alx-intranet.hbtn.io/status')
+    print('Body response:')
+    print('\t- type:', type(response.text))
+    print('\t- content:', response.text)
