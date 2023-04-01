c188dcd94e6564354e1121fe6761295c28fd7d36
Brymo07 <felixowusu479@gmail.com>
2023-03-31 19:16:08 -0700

    network

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -3,14 +3,11 @@
 Usage: ./6-post_email.py <URL> <email>
   - Displays the body of the response.
 """
-import sys
 import requests
-
+import sys
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    value = {"email": sys.argv[2]}
-
-    r = requests.post(url, data=value)
-    print(r.text)
-
+    email = {'email': sys.argv[2]}
+    response = requests.post(url, data=email)
+    print(response.text)
