2fe8af8c077ceed203d91cf8d42710c1e1301b18
Brymo07 <felixowusu479@gmail.com>
2023-03-31 19:18:53 -0700

    network

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -3,16 +3,14 @@
 Usage: ./7-error_code.py <URL>
   - Handles HTTP errors.
 """
-import sys
 import requests
-
+import sys
 
 if __name__ == "__main__":
     url = sys.argv[1]
+    response = requests.get(url)
 
-    r = requests.get(url)
-    if r.status_code >= 400:
-        print("Error code: {}".format(r.status_code))
+    if response.status_code >= 400:
+        print("Error code:", response.status_code)
     else:
-        print(r.text)
-
+        print(response.text)
